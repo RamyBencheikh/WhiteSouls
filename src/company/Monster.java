08d403bb061ff9f222e8cf17d7ddd62bff027f6a
Ramy BENCHEIKH <ramy.bencheikh@ynov.com>
2018-03-06 15:27:32 +0100

    AJout class monster

---
+++ src/company/Monster.java
@@ -0,0 +1,92 @@
+package company;
+
+public class Monster {
+
+    private String name;
+    private int life;
+    private int maxLife;
+    private int stamina;
+    private int maxStamina;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getMaxLife() {
+        return maxLife;
+    }
+
+    public void setMaxLife(int maxLife) {
+        this.maxLife = maxLife;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getMaxStamina() {
+        return maxStamina;
+    }
+
+    public void setMaxStamina(int maxStamina) {
+        this.maxStamina = maxStamina;
+    }
+
+    /**
+     * Define the hero name with the choose of the user
+     * Set 100 maxLife and 50 max Stamina
+     * @param name: Name of the hero
+     */
+    public Monster(String name) {
+        this.name = name;
+        this.life = 10;
+        this.maxLife = 10;
+        this.stamina = 10;
+        this.maxStamina = 10;
+    }
+
+    /**
+     * Define a default hero name when user not define it
+     * Set 100 maxLife and 50 max Stamina
+     */
+    public Monster() {
+        this.name = "Ynovator";
+        this.life = 10;
+        this.maxLife = 10;
+        this.stamina = 10;
+        this.maxStamina = 10;
+    }
+
+    public String toString() {
+        if (isALive()) {
+            return "[Monster]\t" + this.name + "[Stamina]\t" + this.stamina + "[Life]\t" + this.life + "\t (alive)";
+        } else {
+            return "[Monster]\t" + this.name + "[Stamina]\t" + this.stamina + "[Life]\t" + this.life + "\t (dead)";
+        }
+
+    }
+
+    public void printStats() {
+        System.out.println(toString());
+    }
+
+    public Boolean isALive() {
+        return this.life != 0;
+    }
+}
